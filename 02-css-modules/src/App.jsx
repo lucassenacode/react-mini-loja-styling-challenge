74a3e727932fa411c878ad339c5d9b3b58f0c7c4
lucassenacode <lucassenacode@gmail.com>
2025-09-16 14:39:24 -0300

    fix(css-modules): Corrige caminhos das imagens para o deploy

--- 02-css-modules/src/App.jsx
+++ 02-css-modules/src/App.jsx
@@ -3,6 +3,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import ProductGrid from './components/ProductGrid';
 
+
+import imgCadeira from './assets/images/cadeira.jpg';
+import imgFone from './assets/images/fone.jpg';
+import imgGarrafa from './assets/images/garrafa.jpg';
+import imgMochila from './assets/images/mochila.jpg';
+import imgMousepad from './assets/images/mousepad.jpg';
+import imgTenis from './assets/images/tenis.jpg';
+
 function App() {
   const [theme, setTheme] = useState(() => localStorage.getItem('theme') || 'light');
   const [isLoading, setIsLoading] = useState(true);
@@ -25,12 +33,12 @@ function App() {
   };
 
   const productsData = [
-    { id: 1, title: "Tênis Esportivo Pro", price: 350.50, rating: 4.5, tag: "Novo", image: '/src/assets/images/tenis.jpg' },
-    { id: 2, title: "Fone de Ouvido Sem Fio", price: 199.90, rating: 4.8, tag: "Promo", image: '/src/assets/images/fone.jpg' },
-    { id: 3, title: "Mochila para Notebook", price: 250.00, rating: 4.6, tag: null, image: '/src/assets/images/mochila.jpg' },
-    { id: 4, title: "Garrafa Térmica Inox", price: 89.99, rating: 4.9, tag: "Novo", image: '/src/assets/images/garrafa.jpg' },
-    { id: 5, title: "Mousepad Gamer RGB", price: 120.00, rating: 4.7, tag: null, image: '/src/assets/images/mousepad.jpg' },
-    { id: 6, title: "Cadeira de Escritório Ergonômica", price: 1250.00, rating: 4.8, tag: "Promo", image: '/src/assets/images/cadeira.jpg' },
+    { id: 1, title: "Tênis Esportivo Pro", price: 350.50, rating: 4.5, tag: "Novo", image: imgTenis },
+    { id: 2, title: "Fone de Ouvido Sem Fio", price: 199.90, rating: 4.8, tag: "Promo", image: imgFone },
+    { id: 3, title: "Mochila para Notebook", price: 250.00, rating: 4.6, tag: null, image: imgMochila },
+    { id: 4, title: "Garrafa Térmica Inox", price: 89.99, rating: 4.9, tag: "Novo", image: imgGarrafa },
+    { id: 5, title: "Mousepad Gamer RGB", price: 120.00, rating: 4.7, tag: null, image: imgMousepad },
+    { id: 6, title: "Cadeira de Escritório Ergonômica", price: 1250.00, rating: 4.8, tag: "Promo", image: imgCadeira },
   ];
 
   return (
